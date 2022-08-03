c0b5e09a3344a22c4bb94918771bcd47abbcc722
faik-patel-aot <109671472+faik-patel-aot@users.noreply.github.com>
2022-08-03 18:51:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+console.log(secretNumber);
+
+document.querySelector(".check").addEventListener("click", function () {
+  console.log(document.querySelector(".guess").value);
+
+  //===================  game logic ====================//
+
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".start").textContent = "No Number";
+
+    //=========win condition ==========//
+  } else if (guess === secretNumber) {
+    document.querySelector(".title").textContent = "YOU WON";
+    document.querySelector(".start").textContent = "Correct Number";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#37c871";
+
+    //=============scores=================//
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+    
+   //===============loose condition======================//
+
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".start").textContent = "Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }else{
+      document.querySelector(".title").textContent = "YOU LOOSE";
+    document.querySelector(".start").textContent = "Better Luck Next Time";
+    document.querySelector("body").style.backgroundColor = "#ff0000";
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".start").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    }else{
+      document.querySelector(".title").textContent = "YOU LOOSE";
+    document.querySelector(".start").textContent = "Better Luck Next Time";
+    document.querySelector("body").style.backgroundColor = "#ff0000";
+    }
+  } 
+});
+
+
+//====================  again button ======================//
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".start").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#000";
+});
